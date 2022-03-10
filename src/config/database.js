22cfea449aad14f4955fcf724f890c46694e1883
Guilherme Nunes <marcos.nunes@mundiale.com.br>
2022-03-10 19:18:44 -0300

    second commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: '123456',
-    database: 'estoque',
+    host: 'us-cdbr-east-05.cleardb.net',
+    username: 'bb371ca5c72465',
+    password: 'f916f39c',
+    database: 'heroku_f284cb4c468885a',
     port: 3306,
     define: {
         timestamps: true,
